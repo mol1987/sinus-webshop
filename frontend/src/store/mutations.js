05e7e2347de11a967780fc6d6283d812cef91a71
sadqw fwe <fmolen@gmail.com>
2021-02-19 19:21:48 +0100

    small fix

--- frontend/src/store/mutations.js
+++ frontend/src/store/mutations.js
@@ -1,51 +1,66 @@
+import router from '@/router'
+
+
 export default {
     GetAllProducts(state, data) {
         state.productList = data
-    },
-    GetProduct(state, data) {
+      },
+      GetProduct(state, data) {
         state.product = data
-    },
-    AuthenticateUser(state, data) {
+      },
+      AuthenticateUser(state, data) {
         if (data.user == null) {
             state.loginValidation = 'Not found'
         }
         state.userToken = data.token
         state.loggedInUser = data.user
-        console.log('data ',data)
-    },
-    GetAllOrders(state, data) {
-        for (let index = 0; index < data.length; index++) {
-            let temp = []
-      
-            data[index].items.forEach(e => {
-                const product = state.productList.find(f => f._id == e)
-                temp.push(product)
-            })
-            data[index].items = temp
+        if (data.user.role == 'admin') {
+            router.push('Admin')
         }
-
+      },
+      GetAllOrders(state, data) {
+        for (let index = 0; index < data.length; index++) {
+          let temp = [] 
+          
+          data[index].items.forEach(e => {
+              const product = state.productList.find(f => f._id == e)
+              if (product != null) {
+                temp.push(product)
+              } else {
+                temp.push({
+                  title: 'Gretas Fury',
+                  price: 999,
+                  shortDesc: 'Unisex',
+                  longDesc: 'Skate ipsum dolor sit amet...',
+                  imgFile: 'skateboard-greta.png' // Asset logic on clientside
+                })
+              }
+          })
+          data[index].items = temp
+        }
+  
         state.orderList = data
-    },
-    AddOrder(state, data) {
+      },
+      AddOrder(state, data) {
         state.orderList.push(data)
-    },
-    LogoutUser(state) {
+      },
+      LogoutUser(state) {
         state.orderList = {}
         state.loggedInUser = null
         state.userToken = null
-    },
-    AddToCart(state, data) {
+      },
+      AddToCart(state, data) {
         let index = state.cartList.findIndex(f => f.product.serial == data.serial)
         if (index >= 0) {
-            state.cartList[index].amount++;
+          state.cartList[index].amount++;
         } else {
-            state.cartList.push({ product: data, amount: 1 })
+          state.cartList.push({product: data, amount: 1})
         }
-    },
-    CreateOrder(state) {
+      },
+      CreateOrder(state) {
         state.cartList = []
-    },
-    CreateNewProduct(state, data) {
+      },
+      CreateNewProduct(state, data) {
         if (data.data != null) {
           state.productList.push(data.data)
           alert('Product Created!')
@@ -53,11 +68,11 @@ export default {
           alert('Product could not be created')
         }
       },
-    UpdateProduct(state, data) {
+      UpdateProduct(state, data) {
         if (data.data != null) {
           alert('Product edited!')
         } else {
           alert('Product could not be edited')
         }
-      }
+    }
 }
